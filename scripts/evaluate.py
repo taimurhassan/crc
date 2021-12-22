a1f3f2fc8be48f154bf938cd3ac00f3c3c771d0d
Taimur Hassan <70101972+taimurhassan@users.noreply.github.com>
2021-12-23 02:10:01 +0400

    Add files via upload

---
+++ scripts/evaluate.py
@@ -0,0 +1,53 @@
+import sacred
+from sacred import Experiment
+
+from mot_neural_solver.utils.misc import make_deterministic, get_run_str_and_save_dir
+
+from mot_neural_solver.path_cfg import OUTPUT_PATH, DATA_PATH
+import os.path as osp
+
+from mot_neural_solver.pl_module.pl_module import MOTNeuralSolver
+from mot_neural_solver.utils.evaluation import compute_mot_metrics
+
+import pandas as pd
+import scipy.io as sio
+import numpy as np
+import pair_nuclei as pair
+
+from sacred import SETTINGS
+SETTINGS.CONFIG.READ_ONLY_CONFIG=False
+
+ex = Experiment()
+ex.add_config('configs/tracking_cfg.yaml')
+ex.add_config({'run_id': 'evaluation',
+               'add_date': True,
+               'precomputed_embeddings': True})
+
+@ex.automain
+def main(_config, _run):
+
+    print(_config['dataset'])
+    #input()
+    #sacred.commands.print_config(_run) # No need to print config, as it's overwritten by the one from the ckpt.
+    make_deterministic(12345)
+
+    run_str, save_dir = get_run_str_and_save_dir(_config['run_id'], None, _config['add_date'])
+    out_files_dir = osp.join(save_dir, 'mot_files')
+
+    # Load model from checkpoint and update config entries that may vary from the ones used in training
+    model = MOTNeuralSolver.load_from_checkpoint(checkpoint_path=_config['ckpt_path'] if osp.exists(_config['ckpt_path'])  else osp.join(OUTPUT_PATH, _config['ckpt_path']))
+    model.hparams.update({'eval_params':_config['eval_params'],
+                          'data_splits':_config['data_splits']})
+    model.hparams['dataset_params']['precomputed_embeddings'] = _config['precomputed_embeddings']
+    model.hparams['dataset_params']['img_batch_size'] = _config['dataset_params']['img_batch_size']
+
+    #print(model.model)
+    #input()
+    # Get output MOT results files
+    test_dataset = model.test_dataset()
+    constr_satisf_rate = model.track_all_seqs(dataset=test_dataset,
+                                              output_files_dir = out_files_dir,
+                                              use_gt = False,
+                                              verbose=True)
+
+    pair.pair_nuclei_and_generate_output(out_files_dir, _config['dataset'])
